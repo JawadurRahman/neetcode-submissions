2c6a49bb1ae75949241b5c30b8410dc088c2692c
JawadurRahman <79373815+JawadurRahman@users.noreply.github.com>
2026-06-20 20:55:21 -0300

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        PandS = [(p, s, (target - p)/s) for p, s in zip(position, speed)]
+        PandS.sort(key=lambda x: x[0])
+        print(PandS)
+        count = 1
+        stack = []
+        for x in PandS:
+            if not stack:
+                stack.append(x)
+            else:
+                while stack and stack[-1][2] <= x[2]:
+                    stack.pop()
+                stack.append(x)
+                
+            
+        return len(stack)
