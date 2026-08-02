dea2e58ec10c4da8774cab08bce1902b8923bd27
JawadurRahman <79373815+JawadurRahman@users.noreply.github.com>
2026-08-02 11:51:09 -0300

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.py
@@ -0,0 +1,10 @@
+from functools import cache
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        @cache
+        def dfs(index):
+            if index >= len(cost): return 0
+            return cost[index] + min(dfs(index + 1), dfs(index + 2))
+
+        return min(dfs(0), dfs(1))
